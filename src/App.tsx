53de4332cb4c1ff11ef1d22131e58d989a27ab71
JoeIt <josignaciot@gmail.com>
2022-07-09 18:37:59 -0300

    Implement Global State with Context

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { AddNewItem } from './AddNewItem';
 import { Column } from './Column';
 import { AppContainer } from './styles';
+import { useAppState } from './context/AppStateContext';
 
 function App() {
+  const { lists } = useAppState();
+
   return (
     <AppContainer>
-      <Column text="Todo:" />
+      {lists.map(list => (
+        <Column text={list.text} key={list.id} id={list.id} />
+      ))}
       <AddNewItem
         toggleButtonText="+ Add another list"
         onAdd={() => console.log('Item created')}
